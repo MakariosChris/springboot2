c6419cf09e09d2aaa361b9aba44f1025c0e2e216
MakariosChris <fcm007fcm@outlook.com>
2023-05-07 15:08:21 -0300

    criar projeto

---
+++ src/main/java/com/makarioschris/srpingboot2/Srpingboot2Application.java
@@ -0,0 +1,13 @@
+package com.makarioschris.srpingboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Srpingboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Srpingboot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/makarioschris/srpingboot2/Srpingboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarioschris.srpingboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Srpingboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
